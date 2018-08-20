5ac3332437b398e09ec575e8040831415ec6524e
Jason Shuler <jshuler@gmail.com>
2018-08-20 08:53:09 -0400

    fixed classic createpackage with newer nuget command line path

--- libraries/Microsoft.Bot.Builder.Classic/Microsoft.Bot.Builder.Classic/createpackage.cmd
+++ libraries/Microsoft.Bot.Builder.Classic/Microsoft.Bot.Builder.Classic/createpackage.cmd
@@ -10,5 +10,5 @@ msbuild /property:Configuration=release Microsoft.Bot.Builder.Classic.csproj
 msbuild /property:Configuration=release ..\Microsoft.Bot.Builder.Classic.Autofac\Microsoft.Bot.Builder.Classic.Autofac.csproj 
 msbuild /property:Configuration=release ..\rview\rview.csproj
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.Classic.dll).FileVersionInfo.FileVersion"') do set version=%%v
-..\..\..\packages\NuGet.CommandLine.4.1.0\tools\NuGet.exe pack Microsoft.Bot.Builder.Classic.nuspec -symbols -properties version=%version% -OutputDirectory ..\nuget
+..\..\..\packages\NuGet.CommandLine.4.5.1\tools\NuGet.exe pack Microsoft.Bot.Builder.Classic.nuspec -symbols -properties version=%version% -OutputDirectory ..\nuget
 echo *** Finished building Microsoft.Bot.Builder.Classic 
